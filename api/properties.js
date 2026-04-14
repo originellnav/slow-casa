49da1a15a26e9a01aeb6a7c40ca6435d580f95ec
originellnav <54980869+originellnav@users.noreply.github.com>
2026-04-14 10:12:15 +0200

    Add files via upload

---
+++ api/properties.js
@@ -0,0 +1,17 @@
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  var maxRecords = req.query.all ? '100' : '6';
+
+  const response = await fetch(
+    'https://api.airtable.com/v0/appndrnWrdlgxRJAG/Properties?maxRecords=' + maxRecords,
+    {
+      headers: {
+        Authorization: 'Bearer patgpNhgfFkQsyQj9.887202d16495ba49fad025cb888cef3eac0a6c34058675dd2516127ad083d8c6'
+      }
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
