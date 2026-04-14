71825c748131bf495cdfa7afeec27788f04939e9
originellnav <54980869+originellnav@users.noreply.github.com>
2026-04-14 10:26:42 +0200

    Update properties.js

--- api/properties.js
+++ api/properties.js
@@ -7,7 +7,6 @@ module.exports = async function handler(req, res) {
   } else {
     url = 'https://api.airtable.com/v0/appndrnWrdlgxRJAG/Properties?maxRecords=6&filterByFormula={Featured}=1';
   }
-
   const response = await fetch(url, {
     headers: {
       Authorization: 'Bearer patgpNhgfFkQsyQj9.887202d16495ba49fad025cb888cef3eac0a6c34058675dd2516127ad083d8c6'
