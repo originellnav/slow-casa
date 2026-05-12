b8dd364d86d4e6300507c4b9b412ec1c7e454906
originellnav <54980869+originellnav@users.noreply.github.com>
2026-05-12 11:51:52 +0200

    Update property-page.js

--- api/property-page.js
+++ api/property-page.js
@@ -265,7 +265,6 @@ module.exports = async function handler(req, res) {
 </head>
 <body>
 
-  <h1 style="position:absolute;width:1px;height:1px;padding:0;margin:-1px;overflow:hidden;clip:rect(0,0,0,0);border:0;">${name} — ${location}</h1>
 
   <nav>
     <div></div>
