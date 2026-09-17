6fdc3cff612cbdac26fb6a9d6be4a86264bb3fe6
originellnav <54980869+originellnav@users.noreply.github.com>
2026-09-17 12:12:10 +0200

    Update guides-index.js

--- api/guides-index.js
+++ api/guides-index.js
@@ -1,3 +1,4 @@
+const { nav, footer } = require('../lib/nav');
 const SANITY_PROJECT_ID = 'hchp27po';
 const SANITY_DATASET = 'production';
 const SANITY_API_VERSION = '2024-01-01';
@@ -382,16 +383,7 @@ module.exports = async function handler(req, res) {
 </head>
 <body>
 
-  <nav>
-    <div></div>
-    <a href="/" class="wordmark">Slow Casa</a>
-    <ul class="nav-links">
-      <li><a href="/directory">Directory</a></li>
-      <li><a href="/guides">Guides</a></li>
-      <li><a href="https://newsletter.slowcasa.com/subscribe" target="_blank" rel="noopener">Newsletter</a></li>
-      <li><a href="/criteria">About</a></li>
-    </ul>
-  </nav>
+  ${nav()}
 
   <header class="guides-header">
     <h1 class="guides-title">${escapeHtml(page.heading)}</h1>
@@ -402,16 +394,7 @@ module.exports = async function handler(req, res) {
     ${guidesHtml}
   </main>
 
-  <footer>
-    <div class="footer-left">
-      <span class="footer-copy">&copy; 2026 Slow Casa</span>
-      <a href="/privacy" class="footer-policy">Privacy Policy</a>
-    </div>
-    <div class="footer-links">
-      <a href="https://www.instagram.com/theslowcasa/" target="_blank" rel="noopener">Instagram</a>
-      <a href="https://newsletter.slowcasa.com/subscribe" target="_blank" rel="noopener">Newsletter</a>
-    </div>
-  </footer>
+  ${footer()}
 
 </body>
 </html>`;
